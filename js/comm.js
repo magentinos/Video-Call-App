30dff8f5a5793101ca26e673b8ccf84078351377
Roman Chaykovskiy <r.chaykovskiy@gmail.com>
2017-12-17 00:09:01 +0200

    Stun server configuration

--- js/comm.js
+++ js/comm.js
@@ -9,8 +9,16 @@
 
 // {urls: 'stun:stun.l.google.com:19302'}
 var servers = {
-    iceServers: [
-        {urls: 'stun:stun.iptel.org'}
+    iceServers:[
+        {urls: ["turn:173.194.72.127:19305?transport=udp",
+            "turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
+            "turn:173.194.72.127:443?transport=tcp",
+            "turn:[2404:6800:4008:C01::7F]:443?transport=tcp"
+        ],
+            username:"CKjCuLwFEgahxNRjuTAYzc/s6OMT",
+            credential:"u1SQDR/SQsPQIxXNWQT7czc/G4c="
+        },
+        {urls:["stun:stun.l.google.com:19302"]}
     ]
 };
 
