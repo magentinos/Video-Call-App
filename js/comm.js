81176ed773c1982f272ede9c4eee1e4ea48ad926
Roman Chaykovskiy <r.chaykovskiy@gmail.com>
2017-12-17 12:30:26 +0200

    Web RTC configuration

--- js/comm.js
+++ js/comm.js
@@ -9,16 +9,20 @@
 
 // {urls: 'stun:stun.l.google.com:19302'}
 var servers = {
-    iceServers:[
-        {urls: ["turn:173.194.72.127:19305?transport=udp",
-            "turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
-            "turn:173.194.72.127:443?transport=tcp",
-            "turn:[2404:6800:4008:C01::7F]:443?transport=tcp"
-        ],
-            username:"CKjCuLwFEgahxNRjuTAYzc/s6OMT",
-            credential:"u1SQDR/SQsPQIxXNWQT7czc/G4c="
+    'iceServers': [
+        {
+            'urls': 'stun:stun.l.google.com:19302'
         },
-        {urls:["stun:stun.l.google.com:19302"]}
+        {
+            'urls': 'turn:192.158.29.39:3478?transport=udp',
+            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            'username': '28224511:1379330808'
+        },
+        {
+            'urls': 'turn:192.158.29.39:3478?transport=tcp',
+            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            'username': '28224511:1379330808'
+        }
     ]
 };
 
